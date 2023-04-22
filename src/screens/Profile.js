23730576910074f4131a97186d5602bab0682368
Tarkeshwar Prasad Yadav <tarkeshwar@bosframework.com>
2023-04-22 13:47:18 +0530

    Initial commit

---
+++ src/screens/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Profile = () => {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+      }}>
+      <Text style={{}}>Profile</Text>
+    </View>
+  );
+};
+export default Profile;
